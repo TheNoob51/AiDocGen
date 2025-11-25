6cf2fa55fff34ff87925e009c2597460e937bcec
Gyan Vardhan <115806215+TheNoob51@users.noreply.github.com>
2025-11-25 23:40:53 +0530

    api integration

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
 const api = axios.create({
-    baseURL: 'http://localhost:8000', // Backend URL
+    baseURL: backendUrl || 'http://localhost:8000', // Backend URL
     headers: {
         'Content-Type': 'application/json',
     },
